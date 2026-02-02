8abb644434291843fb795dcf899fe448462cca85
annihilation-exe <saikrishnasriram2525@gmail.com>
2026-02-02 19:23:06 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,22 +1,22 @@
-// swift-tools-version: 5.9
+//swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
-    name: "testSDK",
+    name: "InMobiSDK",
     platforms: [
         .iOS(.v15) // Adjust to your minimum deployment target
     ],
     products: [
         // This is the library name the client sees
         .library(
-            name: "testSDK",
-            targets: ["testSDK"]
+            name: "InMobiSDK",
+            targets: ["InMobiSDK"]
         ),
     ],
     targets: [
         // The binary target pointing to your hosted zip
         .binaryTarget(
-            name: "testSDK",
+            name: "InMobiSDK",
             url: "https://dl.inmobi.com/inmobi-sdk/IM/InMobi-iOS-SDK-11.1.1.zip", // <--- YOUR HOSTED URL
             checksum: "578dd32285cc8cea05e04ef3ffd03ccf0c93bc010d1e3abbed28690fe0dfffb2"       // <--- YOUR CHECKSUM
         )
