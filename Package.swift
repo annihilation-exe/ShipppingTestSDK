79e36e32f9a11717e4ec15b11ab9fb0ac630adc0
annihilation-exe <saikrishnasriram2525@gmail.com>
2026-03-30 15:32:34 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
         // The binary target pointing to your hosted zip
         .binaryTarget(
             name: "InMobiSDK",
-            url: "https://dl.inmobi.com/inmobi-sdk/IM/InMobi-iOS-SDK-11.1.1.zip", // <--- YOUR HOSTED URL
-            checksum: "578dd32285cc8cea05e04ef3ffd03ccf0c93bc010d1e3abbed28690fe0dfffb2"       // <--- YOUR CHECKSUM
+            url: "https://supply.inmobicdn.net/sdk/iOS/build/InMobi-iOS-SDK-11.2.0-20260330.065058-2-spm.zip", // <--- YOUR HOSTED URL
+            checksum: "9a9054879dd65659530bcc640bb613fe0d87c41e891a2d1c48eab7489cc3087d"       // <--- YOUR CHECKSUM
         )
     ]
 )
