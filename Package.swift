c7064c0542627e3bf0730bd63e3ac07ad618d9ff
annihilation-exe <saikrishnasriram2525@gmail.com>
2026-03-30 15:45:07 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,24 +1,31 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.3
 import PackageDescription
 
 let package = Package(
     name: "InMobiSDK",
-    platforms: [
-        .iOS(.v15) // Adjust to your minimum deployment target
-    ],
+    platforms: [.iOS(.v12)],
     products: [
-        // This is the library name the client sees
-        .library(
-            name: "InMobiSDK",
-            targets: ["InMobiSDK"]
-        ),
+        .library(name: "InMobiSDK", targets: ["InMobiSDKTarget"])
     ],
     targets: [
-        // The binary target pointing to your hosted zip
         .binaryTarget(
             name: "InMobiSDK",
-            url: "https://supply.inmobicdn.net/sdk/iOS/build/InMobi-iOS-SDK-11.2.0-20260330.065058-2-spm.zip", // <--- YOUR HOSTED URL
-            checksum: "9a9054879dd65659530bcc640bb613fe0d87c41e891a2d1c48eab7489cc3087d"       // <--- YOUR CHECKSUM
+            url: "https://supply.inmobicdn.net/sdk/iOS/build/InMobi-iOS-SDK-11.2.0-20260330.065058-2-spm.zip",
+            checksum: "9a9054879dd65659530bcc640bb613fe0d87c41e891a2d1c48eab7489cc3087d"
+        ),
+        .target(
+            name: "InMobiSDKTarget",
+            dependencies: ["InMobiSDK"],
+            path: "Sources",
+            linkerSettings: [
+                .linkedFramework("WebKit"),
+                .linkedLibrary("sqlite3"),
+                .linkedLibrary("z")
+                
+                // NOTE: Swift Package Manager currently does not allow dependencies with unsafeFlags, unless a specific branch/commit is used.
+                // For now, these flags should be added manually to the project for integration.
+                // .unsafeFlags(["-ObjC"])
+            ]
         )
     ]
 )
