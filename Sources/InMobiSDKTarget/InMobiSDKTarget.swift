5525b4e88c0d4ccf32c7bbff609c837c63fffb37
annihilation-exe <saikrishnasriram2525@gmail.com>
2026-03-30 15:35:07 +0530

    added wrapper

---
+++ Sources/InMobiSDKTarget/InMobiSDKTarget.swift
@@ -0,0 +1,10 @@
+//
+//  InMobiSDKTarget.swift
+//  
+//
+//  Created by Sriram Sai Krishna on 17/03/26.
+//
+
+import Foundation
+// Intentionally empty wrapper target.
+// Used to attach linker settings and depend on the InMobiSDK binary target.
